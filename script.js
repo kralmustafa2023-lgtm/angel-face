296917fb32ecea066cbbe365aa4a0b8ae5e9ef53
kralmustafa2023-lgtm <kralmustafa2023@gmail.com>
2026-05-25 01:31:45 +0300

    Update read receipts icon to double ticks and bump cache version to v=4

--- script.js
+++ script.js
@@ -996,7 +996,8 @@ window.bootChatEngine = function () {
             ticksHtml = `
             <span class="chat-msg__status ${seenClass}">
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <polyline points="20 6 9 17 4 12"></polyline>
+                    <path d="M18 6L7 17l-5-5"/>
+                    <path d="M22 10l-5.5 5.5"/>
                 </svg>
             </span>`;
         }
